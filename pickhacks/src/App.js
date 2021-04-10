c6419121021ecd9ff12396d9e73c6009b3294b03
VemmyRM <kwvembu@gmail.com>
2021-04-10 12:22:32 -0400

    adding modal form to option component

--- pickhacks/src/App.js
+++ pickhacks/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Options from "./Components/Option";
+import Modal from "./Components/Modal";
 import firebase from "firebase";
 
 function App() {
-  function handleFriends (name, email) {
+  function handleFriends(name, email) {
     console.log("you picked friends");
-   database.ref('users/'+name).set({
+    database.ref("users/" + name).set({
       username: name,
-      email: email
+      email: email,
     });
   }
   const firebaseApp = firebase.apps[0];
@@ -16,21 +17,24 @@ function App() {
 
   var username = "PTK";
   var email = "ptk@gmail.com";
-  function handleAlone(){
+  function handleAlone() {
     console.log("you picked alone :(");
   }
   return (
     <div className="App">
       <div className="container">
         <h1 className="title">Welcome to THE APP. Pick your experience. </h1>
-      <div className="row">
-      <div className="col">
-            <Options phrase="Watching with Friends" handleClick={()=>handleFriends(username, email)} />
+        <div className="row">
+          <div className="col">
+            <Options
+              phrase="Watching with Friends"
+              handleClick={() => handleFriends(username, email)}
+            />
           </div>
           <div className="col">
-            <Options phrase="Find me a Movie Buddy" handleClick={handleAlone}/>
+            <Options phrase="Find me a Movie Buddy" handleClick={handleAlone} />
           </div>
-      </div>
+        </div>
       </div>
     </div>
   );
--- pickhacks/src/Components/Option.js
+++ pickhacks/src/Components/Option.js
@@ -1,10 +1,144 @@
+const Options = ({ phrase, handleClick }) => {
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn btn-primary option"
+        data-toggle="modal"
+        data-target="#exampleModalCenter"
+      >
+        {phrase}
+      </button>
 
-const Options = ({phrase, handleClick}) => {
-    return (
-        <div className="option container" onClick={handleClick}>
-            <h1>{phrase}</h1>
+      <div
+        className="modal fade"
+        id="exampleModalCenter"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalCenterTitle">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="form-group">
+                  <label for="watcher-name" className="col-form-label">
+                    What's your name?
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="watcher-name"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <p>Which steaming services do you have?</p>
+                  <div className="form-check form-check-inline">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="inlineRadioOptions"
+                      id="Netflix"
+                      value="Netflix"
+                    />
+                    <label className="form-check-label" for="Netflix">
+                      Netflix
+                    </label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="inlineRadioOptions"
+                      id="Prime Video"
+                      value="Prime Video"
+                    />
+                    <label className="form-check-label" for="Prime Video">
+                      Prime Video
+                    </label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="inlineRadioOptions"
+                      id="Hulu"
+                      value="Hulu"
+                    />
+                    <label className="form-check-label" for="inlineRadio3">
+                      Hulu
+                    </label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="inlineRadioOptions"
+                      id="Disney Plus"
+                      value="Disney Plus"
+                    />
+                    <label className="form-check-label" for="Disney Plus">
+                      Disney Plus
+                    </label>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label for="genreSelect">
+                    Which genres are you interested in?
+                  </label>
+                  <select multiple className="form-control" id="genreSelect">
+                    <option>All</option>
+                    <option>Action</option>
+                    <option>Adventure</option>
+                    <option>Romance</option>
+                    <option>Comedy</option>
+                    <option>Horror</option>
+                    <option>Sci-Fi</option>
+                  </select>
+                </div>
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <button className="btn btn-outline-secondary" type="button">
+                      Generate Link!
+                    </button>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="watcher-name"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Start
+              </button>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Options;
