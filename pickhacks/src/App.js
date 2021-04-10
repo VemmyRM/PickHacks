5bc3def9f7407723fdb4c5264ff096afc0ca4c0c
Prithika Hariharan <prithika.hariharan@gmail.com>
2021-04-10 11:34:29 -0400

    Added database functionality

--- pickhacks/src/App.js
+++ pickhacks/src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import Options from "./Components/Option"
+import Options from "./Components/Option";
+import firebase from "firebase";
 
 function App() {
-  function handleFriends () {
+  function handleFriends (name, email) {
     console.log("you picked friends");
+   database.ref('users/'+name).set({
+      username: name,
+      email: email
+    });
   }
+  const firebaseApp = firebase.apps[0];
+  var database = firebase.database();
 
+  var username = "PTK";
+  var email = "ptk@gmail.com";
   function handleAlone(){
     console.log("you picked alone :(");
   }
   return (
     <div className="App">
       <div className="container">
-        <h1 className="title">Welcome to THE APP. Pick your experience.</h1>
+        <h1 className="title">Welcome to THE APP. Pick your experience. </h1>
       <div className="row">
       <div className="col">
-            <Options phrase="Watching with Friends" handleClick={handleFriends}/>
+            <Options phrase="Watching with Friends" handleClick={()=>handleFriends(username, email)} />
           </div>
           <div className="col">
             <Options phrase="Find me a Movie Buddy" handleClick={handleAlone}/>
--- pickhacks/src/index.js
+++ pickhacks/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDXzIjZWmelWl_ZmpIWGK6QXETYafWULFE",
+  authDomain: "pickhacks-91a97.firebaseapp.com",
+  projectId: "pickhacks-91a97",
+  storageBucket: "pickhacks-91a97.appspot.com",
+  messagingSenderId: "653766672377",
+  appId: "1:653766672377:web:256092378407cb062c0728",
+  measurementId: "G-570D6CWLD8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
